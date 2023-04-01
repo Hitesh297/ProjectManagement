17593416567342cb23ef6b7e5510c67901777d68
hitesh <hites.297@gmail.com>
2023-04-01 00:53:31 -0400

    Consultant create complete

--- ProjectManagement/Models/Client.cs
+++ ProjectManagement/Models/Client.cs
@@ -1,9 +1,14 @@
-﻿namespace ProjectManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace ProjectManagement.Models
 {
     public class Client
     {
         public int Id { get; set; }
+        [Display(Name = "Client Name")]
         public string ClientName { get; set; }
+        [Display(Name = "Is Active")]
         public bool IsActive { get; set; }
     }
 }
--- ProjectManagement/Models/Consultant.cs
+++ ProjectManagement/Models/Consultant.cs
@@ -1,30 +1,42 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
 
 namespace ProjectManagement.Models
 {
     public class Consultant
     {
         public int Id { get; set; }
+        [Display(Name = "Consultant Id")]
         public string UniqueConsultantId { get; set; }
+        [Display(Name = "Consultant Name")]
         public string Name { get; set; }
-        [ForeignKey("Recruiter"), Column(Order = 0)]
+        [Display(Name = "Recruiter")]
+        [ForeignKey("Recruiter")]
         public int? RecruiterMemberId { get; set; }
-        [ForeignKey("TeamLead"), Column(Order = 1)]
+        [Display(Name = "Team Lead")]
+        [ForeignKey("TeamLead")]
         public int? TeamLeadMemberId { get; set; }
+        [Display(Name = "Marketing Manager")]
         [ForeignKey("MarketingManager")]
         public int? MarketingManagerMemberId { get; set; }
+        [Display(Name = "Team Member")]
         [ForeignKey("TeamMember")]
         public int? TeamMemberId { get; set; }
+        [Display(Name = "Referred By")]
         [ForeignKey("ReferredBy")]
         public int? ReferredByMemberId { get; set; }
+        [Display(Name = "Placed By")]
         [ForeignKey("PlacedBy")]
         public int? PlacedByMemberId { get; set; }
         public virtual TeamMember? Recruiter { get; set; }
         public virtual TeamMember? TeamLead { get; set; }
 
         [Column(TypeName = "Date")]
+        [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
         [Column(TypeName = "Date")]
+        [DataType(DataType.Date)]
         public DateTime? EndDate { get; set; }
 
         public string Status
@@ -41,21 +53,29 @@ namespace ProjectManagement.Models
         }
 
 
-        //client
+        [Display(Name = "Client")]
         [ForeignKey("Client")]
         public int? ClientId { get; set; }
         public virtual Client? Client { get; set; }
         public TeamMember? TeamMember { get; set; }
+        [Display(Name = "Billing Rate")]
         public decimal BillingRate { get; set; }
+        [Display(Name = "Pay Rate")]
         public decimal PayRate { get; set; }
+        [Display(Name = "Team Lead Fee")]
         public decimal TeamLeadFee { get; set; }
+        [Display(Name = "Marketing Fee")]
         public decimal MarketingFee { get; set;}
         public TeamMember? MarketingManager { get; set; }
+        [Display(Name = "Referral Fee")]
         public decimal ReferralFees { get; set; }
         public TeamMember? ReferredBy { get; set; }
+        [Display(Name = "Placement Fee")]
         public decimal PlacementFee { get; set; }
         public TeamMember? PlacedBy { get; set; }
+        [Display(Name = "Credit Card Cost")]
         public decimal CreditCardCost { get; set; }
+        [Display(Name = "Net Margin")]
         public decimal NetMargin { get; set; }
     }
 }
